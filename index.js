7a21d9bf43774fae523f9d844208c1019bed791d
Luis Alberto Ramírez Bermúdez <77942733+luisinho07@users.noreply.github.com>
2021-07-14 02:23:41 +0000

    clase 2

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
-// introducción a Javascript
+// introducción a Javascript clase 1
 
-let dos;
+//let dos;
 
-dos = 2;
-console.log(dos);
+//dos = 2;
+//console.log(dos);
 
-let str1 = "hola";
-let str2 = "mundo";
+//let str1 = "hola";
+//let str2 = "mundo";
 
 // let allString = str1 + " " + str2;
-let allString = `${str1}  ${str2}`;
+//let allString = `${str1}  ${str2}`;
 
 
-console.log(allString)
+//console.log(allString)
 
 
-let car1 = 1;
-let car2 = "2";
-console.log(car1 + car2)
+//let car1 = 1;
+//let car2 = "2";
+//console.log(car1 + car2)
 
-let car = 1;
-console.log(car);
-car = "Renault";
+//let car = 1;
+//console.log(car);
+//car = "Renault";
 
 //let edad = prompt("Digita tu edad");
 //let suma = parseInt(edad)+2;
 //console.log(suma);
 
-let num1 = prompt("Digita primer número");
-let num2 = prompt("Digita segundo número");
-let suma = parseInt(num1) + parseInt(num2);
-console.log(`La suma de ${num1} y ${num2} es: ${suma}`);
+//let num1 = prompt("Digita primer número");
+//let num2 = prompt("Digita segundo número");
+//let suma = parseInt(num1) + parseInt(num2);
+//console.log(`La suma de ${num1} y ${num2} es: ${suma}`);
 
-let anioNacimiento = prompt("Digita el año de tu nacimiento");
-edad = 2021 - parseInt(anioNacimiento);
-console.log(`Para el año 2021 tendrás: ${edad}`);
+//let anioNacimiento = prompt("Digita el año de tu nacimiento");
+//edad = 2021 - parseInt(anioNacimiento);
+//console.log(`Para el año 2021 tendrás: ${edad}`);
 
-let peso = prompt("Digita tu peso en kilogramos: ");
-let estatura = prompt("Digita tu estatura en metros: ");
-let imc = parseInt(peso)/(parseFloat(estatura)**2);
-console.log(`Tu BMI es: ${imc}`)
+//let peso = prompt("Digita tu peso en kilogramos: ");
+//let estatura = prompt("Digita tu estatura en metros: ");
+//let imc = parseInt(peso)/(parseFloat(estatura)**2);
+//console.log(`Tu BMI es: ${imc}`)
+
+let numero = prompt("Digita un número: ");
+if (parseInt(numero) < 10) {
+  
+}
